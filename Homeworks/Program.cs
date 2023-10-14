029e3549315a01acc24782781088bd254d0161ca
RomanK <roman_krasnoruckiy@mail.ru>
2023-10-14 14:09:24 +0300

    написал 4 задачу

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -38,3 +38,116 @@
 //     }
 // }
 
+// Внутри класса Answer напишите метод FindMax, 
+// который принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// using System;
+
+// public class Answer {
+    
+//   static int FindMax(int a, int b, int c)
+//     {
+//        int max = a;
+//         if (max < b)
+//         {
+//             max = b;
+//         }
+//         if (max < c)
+//         {
+//             max = c;
+//         }
+//         return max;
+
+//     }
+
+//   // Не удаляйте и не меняйте метод Main! 
+//   static public void Main(string[] args) {
+//         int a, b, c;
+
+//         if (args.Length >= 3) {
+//            a = int.Parse(args[0]);
+//            b = int.Parse(args[1]);
+//            c = int.Parse(args[2]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//            a = 5;
+//            b = 6;
+//            c = 7;
+//         }
+
+//         // Не удаляйте строки ниже
+//         int result = FindMax(a, b, c);
+//         System.Console.WriteLine($"{result}");
+//     }
+// }
+
+// Внутри класса Answer напишите метод CheckIfEven,который на вход принимает число number и
+// выводит, является ли число чётным (делится ли оно на два без остатка).
+
+// using System;
+
+// class Answer {
+//     static void CheckIfEven(int number)
+//     {
+//       if(number % 2 == 1)
+//       {
+//          Console.WriteLine($"Число `{number}` нечётное");
+//       }else
+//       {
+//         Console.WriteLine($"Число `{number}` чётное");
+//       }
+
+
+//     }
+  
+
+//   // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args) {
+//         int number;
+
+//         if (args.Length >= 1) {
+//             number = int.Parse(args[0]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 17;
+//         }
+
+//         // Не удаляйте строки ниже
+//         CheckIfEven(number);
+//     }
+// }
+
+//  Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число
+//  (number), а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком
+//  табуляции.
+
+// using System;
+// using System.Data;
+
+// public class Answer {
+//     static void PrintEvenNumbers(int number)
+//     {
+//        for (int count = 2; count <= number; count += 2)
+//         {
+//             Console.Write($"{count}\t");
+//         }
+          
+//     }
+
+
+//   // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args) {
+//         int number;
+
+//         if (args.Length >= 1) {
+//             number = int.Parse(args[0]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 17;
+//         }
+
+//         // Не удаляйте строки ниже
+//         PrintEvenNumbers(number);
+//     }
+// }
+
