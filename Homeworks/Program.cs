f90096361e6995f17de43b81fb4b872f4fc2ce74
RomanK <roman_krasnoruckiy@mail.ru>
2023-10-24 21:40:05 +0300

    начало решения дз

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -248,3 +248,42 @@
 // }
 // ThirdDigit();
 
+
+// Внутри класса Answer напишите метод IsPalindrome, который принимает на вход пятизначное число 
+// number и проверяет, является ли оно палиндромом. Метод должен проверить является ли число пятизначным,
+// в противном случае - вывести Число не пятизначное и False в следующей строке.
+// Для остальных чисел вернуть True или False.
+
+// 14212 -> False
+// 12821 -> True
+// 234322 -> Число не пятизначное
+//                      False
+
+// ПРИНИМАЕТ число!!! ПРОВЕРЯЕТ:  1) палиндром 2) 5 знач.!!! 2 МЕТОДА
+
+using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+      
+    }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 64546;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
+
