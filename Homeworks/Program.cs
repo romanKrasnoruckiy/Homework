b1a3cb2e74bdc55d6664b2a0208738306f179632
RomanK <roman_krasnoruckiy@mail.ru>
2023-10-27 01:03:44 +0300

    доделать задачу №2

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -360,6 +360,75 @@
 // }
 
 
+// Внутри класса Answer напишите метод DistanceBetweenPoints, который принимает на вход координаты двух 
+// точек pointA и pointB в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7) -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+using System;
+
+public class Answer
+{
+    private static double Length(int[] pointA, int[] pointB)
+    {
+      // Введите свое решение ниже
+       
+        double FindDistance(double x1, double x2,double x3, double y1, double y2, double y3)
+        {
+        double result = Math.Round(Math.Sqrt(Math.Pow(y1 - x1, 2) + Math.Pow(y2 - x2, 2) + Math.Pow(y3 - x3, 2)), 2);
+        return result;
+        // или: return Math.Round(Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2)), 2);
+        }
+
+        // Console.Write("Input XA: ");
+        // double xa = Convert.ToDouble(Console.ReadLine());
+        // Console.Write("Input YA: ");
+        // double ya = Convert.ToDouble(Console.ReadLine());
+        // Console.Write("Input ZA: ");
+        // double za = Convert.ToDouble(Console.ReadLine());
+        // Console.Write("Input XB: ");
+        // double xb = Convert.ToDouble(Console.ReadLine());
+        // Console.Write("Input YB: ");
+        // double yb = Convert.ToDouble(Console.ReadLine());
+        // Console.Write("Input ZB: ");
+        // double zb = Convert.ToDouble(Console.ReadLine());
+
+
+
+        Console.WriteLine($"A({x1}, {x2}, {x3}), B({y1}, {y2}, {y3}) -> {FindDistance(x1, x2, x3, y1, y2, y3)}");
+
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+}
+
+
 
 
 
