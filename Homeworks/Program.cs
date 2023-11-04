83eb0c15b17e4b16c4bdd7f24b829afd72a4356b
RomanK <roman_krasnoruckiy@mail.ru>
2023-11-04 18:20:44 +0300

    ДЗ, решение задачи N41(1) по семнару от 22.10.23

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -736,3 +736,49 @@
 // }
 
 
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+// int[] NewArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write("Input number: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+//  {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void FindPosNumber(int[] array)
+// {
+//     int sumPos = 0;
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] > 0) sumPos += 1;
+//     }
+//     Console.WriteLine($"Пользователь ввёл чисел больше 0: -> {sumPos}");
+        
+
+// }
+
+// Console.Write("Input array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = NewArray(size);  
+// PrintArray(myArray);
+// FindPosNumber(myArray);
+
+
