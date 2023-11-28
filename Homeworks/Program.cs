de085788d593b41886258afa5a424a9bc6ace028
RomanK <roman_krasnoruckiy@mail.ru>
2023-11-28 13:57:22 +0300

    задачи N64,66,68 по семинару от 18.11.23

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -802,3 +802,101 @@
 // Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 
 
+
+
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Console.Clear();
+// int Prompt(string message)
+// {
+//   Console.Write(message);
+//   int result = Convert.ToInt32(Console.ReadLine());
+//   return result;
+// }
+
+// int NaturalNumber(int n, int m)
+// {
+//   if (n == m) return n;
+//   else Console.Write($"{NaturalNumber(n, m + 1)}, ");
+//   return m;
+// }
+
+// int n = Prompt("Input N: ");
+// if (n < 1)
+// {
+//   Console.WriteLine("Ввдите положительное число!");
+//   return;
+// }
+// Console.WriteLine(NaturalNumber(n, 1));
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// SumFromMToN(m, n);
+
+// // вызов функции "сумма чисел от M до N"
+// void SumFromMToN(int m, int n)
+// {
+//     Console.Write(SumMN(m - 1, n));
+// }
+
+// // функция сумма чисел от M до N
+// int SumMN(int m, int n)
+// {
+//     int res = m;
+//     if (m == n)
+//         return 0;
+//     else
+//     {
+//         m++;
+//         res = m + SumMN(m, n);
+//         return res;
+//     }
+// }
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// AkkermanFunction(m,n);
+
+
+// // вызов функции Аккермана
+// void AkkermanFunction(int m, int n)
+// {
+//     Console.Write(Akkerman(m, n)); 
+// }
+
+// // функция Аккермана
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
